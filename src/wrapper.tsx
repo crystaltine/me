25e0c0b2240cdb1c7f7987448c0fa867321f1999
crystaltine <donaldcenaaa@outlook.com>
2024-09-15 17:03:12 -0400

    attempt using HashRouter

--- src/wrapper.tsx
+++ src/wrapper.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 
 import './styles/general/index.css'
 import './styles/general/fonts.css'
@@ -8,8 +8,8 @@ import Main from './main'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <Main />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
 )
