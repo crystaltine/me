de47f64d8c0e2979fd5b894ea1d13548f5402687
crystaltine <donaldcenaaa@outlook.com>
2024-09-22 02:49:53 -0400

    Update resume, round preview img corners

--- src/pages/resume.tsx
+++ src/pages/resume.tsx
@@ -15,7 +15,7 @@ import checkIcon from '../assets/svgs/check.svg';
 
 import resumePreview from '../content/resume/resume_preview.png';
 
-const resumeTimestamp = "20th September 2024";
+const resumeTimestamp = "22th September 2024";
 
 const ResumePage = () => {
 	
